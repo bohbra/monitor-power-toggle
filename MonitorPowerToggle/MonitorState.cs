a4ff1b237bff18a8a53a7b43b47b2f17ed8bbce0
Robert Gaede <bobgaede@gmail.com>
2015-03-08 15:47:40 -0700

    Add interop call and args to toggle monitor power

---
+++ MonitorPowerToggle/MonitorState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonitorPowerToggle
+{
+    public enum MonitorState
+    {
+        MonitorStateOn = -1,
+        MonitorStateOff = 2,
+        MonitorStateStandBy = 1
+    }
+}
--- MonitorPowerToggle/Program.cs
+++ MonitorPowerToggle/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace MonitorPowerToggle
 {
@@ -10,6 +10,27 @@ namespace MonitorPowerToggle
     {
         static void Main(string[] args)
         {
+            var arg = args.FirstOrDefault();       
+
+            if (arg == null)
+            {
+                System.Console.WriteLine("Please enter a valid argument");
+                Console.Read();
+                return;
+            }
+
+            var monitorState = arg == "-on" ? MonitorState.MonitorStateOn : MonitorState.MonitorStateOff;
+            
+            // turn monitor off
+            SetMonitorState(monitorState);
+        }
+        
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+        private static void SetMonitorState(MonitorState state)
+        {
+            SendMessage(0xFFFF, 0x112, 0xF170, (int)state);
         }
     }
 }
